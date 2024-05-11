a295321d962af8544fca22d9e4efb1ad3452141f
fordat <fordatwater@gmail.com>
2024-05-11 13:47:12 -0700

    update

---
+++ app/code/repos/[name]/page.jsx
@@ -0,0 +1,9 @@
+const RepoPage = ({ params: { name } }) => {
+  return (
+    <div className="card">
+      <h2>{name}</h2>
+    </div>
+  )
+}
+
+export default RepoPage;
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,5 @@
 import { Poppins } from "next/font/google";
-import { Inter } from "next/font/google";
+import Header from "./components/Header";
 import "./globals.css";
 
 // const inter = ({ subsets: ["latin"] });
@@ -18,6 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={poppins.className}>
+        <Header />
         <main className="container">
           {children}
         </main>
