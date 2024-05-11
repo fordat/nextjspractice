c2b3c26cb7f79a5c342d5c929f092ba927e7d718
fordat <fordatwater@gmail.com>
2024-05-11 16:53:47 -0700

    completed course

--- app/api/courses/route.js
+++ app/api/courses/route.js
@@ -1,6 +1,25 @@
 import { NextResponse } from "next/server";
 import courses from "./data.json";
+import { v4 as uuidv4 } from "uuid";
 
 export async function GET(request) {
+  return NextResponse.json(courses);
+}
+
+export async function POST(request) {
+  const { title, description, level, link } = await request.json();
+
+  const newCourse = {
+    id: uuidv4(),
+    title,
+    description,
+    level,
+    link
+  }
+
+  console.log(newCourse);
+
+  courses.push(newCourse);
+
   return NextResponse.json(courses);
 }
--- app/components/Courses.jsx
+++ app/components/Courses.jsx
@@ -1,27 +1,21 @@
 import Link from "next/link";
 
-async function fetchCourses() {
-  const response = await fetch(
-    "http://localhost:3000/api/courses"
-  );
-  const courses = await response.json();
-  return courses;
-}
-
-const Courses = async () => {
-  const courses = await fetchCourses();
-  return <div className="courses">
-    {courses.map((course) => {
-      <div key={course.id} className="card">
-        <h2>{course.title}</h2>
-        <small>Level: {course.level}</small>
-        <p>{course.description}</p>
-        <Link href={course.link} target="_blank" className="btn">
-          Go To Course
-        </Link>
-      </div>
-    })}
-  </div>
+const Courses = ({ courses }) => {
+  
+  return (
+    <div className="courses">
+      {courses.map((course) => (
+        <div key={course.id} className="card">
+          <h2>{course.title}</h2>
+          <small>Level: {course.level}</small>
+          <p>{course.description}</p>
+          <Link href={course.link} target="_blank" className="btn">
+            Go To Course
+          </Link>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default Courses;
